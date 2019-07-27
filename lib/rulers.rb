919143a2abf68a3b703f1e99ee2bf2610c261a74
hss15 <hari@hss>
2019-07-27 11:27:02 +0530

    adds rack

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,5 +2,10 @@ require "rulers/version"
 
 module Rulers
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ['Game Over from Ruby on Rulers!']]
+    end
+  end
 end
